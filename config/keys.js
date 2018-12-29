3861064406f6dd605e0289cb641964838a146b97
laraismael1 <laraismael123@gmail.com>
2018-12-29 10:37:55 -0800

    starting server

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: "
+mongodb://lara.harrow:cavalo10@ds147723.mlab.com:47723/loginsignup
+"
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// bodyParser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(bodyParser.json());
+
+// DB config
+const db = require("config/keys").mongoURI;
+
+// connect to MongoDB
+mongoose
+  .connect(
+    db, { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch( err => console.log(err));
+
+const port = process.env.PORT || 5000;
+// process.env.PORT is the setup for future deployment with Heroku
+
+app.listen(port, () => console.log(`The server is running on port ${port}`));
